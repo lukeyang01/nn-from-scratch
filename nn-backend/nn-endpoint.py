d1f9fd398d3dd51bca4b7dc12e61cb71219ee0ab
lukeyang01 <lukeyang@umich.edu>
2024-04-13 13:49:56 -0400

    cc progress

--- nn-backend/nn-endpoint.py
+++ nn-backend/nn-endpoint.py
@@ -22,38 +22,18 @@ def index():
     response = flask.jsonify({"endpoints": ["/api/query_mnist/"]})
     return response
 
-@app.route("/api/query_mnist", methods=['POST'])
-def query_mnist():
-    mnist_svg = flask.request.get_json()["mnist_svg"]
-    mem = io.BytesIO()
-    cairosvg.svg2png(mnist_svg, output_width=28, output_height=28, write_to=mem, negate_colors=True)
-
-    x = np.array(Image.open(mem))
-
-    # Only consider first three channels (r,g,b), then convert to grayscale
-    x = x[:,:,:3]
-    x = x.mean(axis=2)
-
-    # Calculate center of mass to center images
-    center_x = np.argmax(x.mean(axis=1))
-    center_y = np.argmax(x.mean(axis=0))
-    x = shift_vector(x, center_x, center_y, 28, 28)
-
-    # Flatten image, normalize
-    x = x.flatten().reshape(784, 1)
-    x = x / 255
-
-    # Generate prediction
-    y_pred, _, _ = mnist.forward(x)
-    # print(y_pred)
-    response = flask.jsonify({"0": str(y_pred[0][0]),
-                              "1": str(y_pred[1][0]),
-                              "2": str(y_pred[2][0]),
-                              "3": str(y_pred[3][0]),
-                              "4": str(y_pred[4][0]),
-                              "5": str(y_pred[5][0]),
-                              "6": str(y_pred[6][0]),
-                              "7": str(y_pred[7][0]),
-                              "8": str(y_pred[8][0]),
-                              "9": str(y_pred[9][0])})
+@app.route("/api/query_cc", methods=['GET'])
+def query_cc():
+    randi = np.random.randint(0, cc_data.shape[0])
+    x_train = cc_norm_data[randi]
+    x = cc_data[randi]
+    # print(x)
+    y_pred, _, _ = cc.forward(x_train)
+    y_pred = np.argmax(y_pred)
+    if (y_pred == 1):
+        y_pred = "Fraud"
+    else:
+        y_pred = "Valid"
+    date = random_date()
+    response = flask.jsonify({"fraud": y_pred, "amount": x[-1], "name": censusname.generate(), "date": date})
     return response
